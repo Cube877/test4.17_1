563cadd7d22ac7cd4b6e4034956ac85704dbf95f
Cube <877211593@qq.com>
2020-04-17 15:05:58 +0800

    wu

--- test4.17_1/test4.17_1.cpp
+++ test4.17_1/test4.17_1.cpp
@@ -2,10 +2,48 @@
 //
 
 #include <iostream>
+#include<opencv.hpp>
+
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat srcMat = imread("E:\\Picture\\rim.png");
+	Mat grayMat;
+	Mat srcMat_b;
+	Mat labels, stats, centroids;
+	cvtColor(srcMat, grayMat, COLOR_BGR2GRAY);
+	threshold(grayMat, srcMat_b, 100, 255, THRESH_OTSU);
+	int height = srcMat_b.rows;
+	int width = srcMat_b.cols;
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			srcMat_b.at<uchar>(j, i) = 255 - srcMat_b.at<uchar>(j, i);
+		}
+	}
+	Mat dst = srcMat.clone();
+	int t = connectedComponentsWithStats(srcMat_b, labels, stats, centroids);
+	for (int i = 1; i < t; i++)
+	{
+		Rect bndbox;
+		bndbox.x = stats.at<int>(i, 0);
+		bndbox.y = stats.at<int>(i, 1);
+		bndbox.width = stats.at<int>(i, 2);
+		bndbox.height = stats.at<int>(i, 3);
+		double p = bndbox.width / bndbox.height;
+		if (p > 0.9 && p < 1.1 && stats.at<int>(i, 4) > 1000 && stats.at<int>(i, 4) < 10000)
+		{
+			rectangle(dst, bndbox, CV_RGB(255, 0, 0), 1, 8, 0);
+		}
+	}
+
+	imshow("srcMat", srcMat);
+	imshow("srcMat_b", srcMat_b);
+	imshow("dst", dst);
+	waitKey();
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
